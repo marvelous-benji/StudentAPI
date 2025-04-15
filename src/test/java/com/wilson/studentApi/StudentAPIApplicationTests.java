c9daf0d164aad6ceaf3dbd5e97ffa132fdfee3b5
Benji Wilson <mac@MACs-MacBook-Pro.local>
2025-04-15 19:36:00 +0100

    initial commit

---
+++ src/test/java/com/wilson/studentApi/StudentAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.studentApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
